ef230386456405540cbc43288ba531c5c5143d2d
kapil jhajhria <jhajhria44@gmail.com>
2021-02-24 21:45:43 +0530

    currying

--- src/index.js
+++ src/index.js
@@ -6,5 +6,14 @@ let output = "<div>" + input.trim() + "</div>";
 const trim = (str) => str.trim();
 
 const wrapInDiv = (str) => "<div>" + str + "</div>";
+const wrapInSpan = (str) => "<span>" + str + "</span>";
+const wrap = (type) => (str) => `<${type}>${str}</${type}>`;
+const toLowerCase = (str) => str.toLowerCase();
 
-console.log(wrapInDiv(trim(input)));
+const transform = compose(wrapInDiv, toLowerCase, trim);
+const transform2 = pipe(trim, toLowerCase, wrap("span"));
+
+const result = wrapInDiv(toLowerCase(trim(input)));
+// console.log(transform(input));
+console.log(transform2(input));
+console.log("hahahaha");
