b4d2373f5fa52e6a299985cd1b012a2c75bdf2cc
kapil jhajhria <jhajhria44@gmail.com>
2021-02-24 21:48:35 +0530

    pure functions

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
 import { compose, pipe } from "lodash/fp";
 
-let input = "  JavaScript    ";
-let output = "<div>" + input.trim() + "</div>";
+//not pure function as it will return diff result each time
+//can't use random value, date etc in pure function
+function myFunction(number) {
+  return number * Math.random();
+}
 
-const trim = (str) => str.trim();
+console.log(myFunction(5));
+console.log(myFunction(5));
+console.log(myFunction(5));
 
-const wrapInDiv = (str) => "<div>" + str + "</div>";
-const wrapInSpan = (str) => "<span>" + str + "</span>";
-const wrap = (type) => (str) => `<${type}>${str}</${type}>`;
-const toLowerCase = (str) => str.toLowerCase();
+//Pure function as it will always return same output for given input
+function myPureFunction(number) {
+  return number * 2;
+}
+const minAge = 18;
 
-const transform = compose(wrapInDiv, toLowerCase, trim);
-const transform2 = pipe(trim, toLowerCase, wrap("span"));
+//if we change minAge then it will no longer be pure function
+function isEligible(age) {
+  return age > minAge;
+}
 
-const result = wrapInDiv(toLowerCase(trim(input)));
-// console.log(transform(input));
-console.log(transform2(input));
-console.log("hahahaha");
+//now its a pure function as it will return same output for given set of input
+
+function isEligible2(age, minAge) {
+  return age > minAge;
+}
