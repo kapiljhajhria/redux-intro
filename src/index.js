c25a9b644cadec70e4ff3dbe4f3e1e5ef46b9eb5
kapil jhajhria <jhajhria44@gmail.com>
2021-02-26 21:21:06 +0530

    ducks pattern for structuring redux files

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-import store from "./store";
-import * as actions from "./actionTypes";
-import { bugAdded, bugRemoved, bugResolved } from "./actionsCreators";
+import configureStore from "./store/configureStore";
+import * as actions from "./store/bugs";
+
+const store = configureStore();
+
 const usubscribe = store.subscribe(() => {
   console.log("state changed", store.getState());
 });
 
 // console.log(store.getState());
 
-store.dispatch(bugAdded("bug 01"));
+store.dispatch(actions.bugAdded("bug 01"));
 // console.log(store.getState());
 // usubscribe();
-store.dispatch(bugAdded("bug 02"));
-store.dispatch(bugRemoved(1));
+store.dispatch(actions.bugAdded("bug 02"));
+store.dispatch(actions.bugRemoved(1));
 // console.log(store.getState());
-store.dispatch(bugResolved(2));
+store.dispatch(actions.bugResolved(2));
--- src/store.js
+++
@@ -1,7 +0,0 @@
-const { createStore } = require("redux");
-import { devToolsEnhancer } from "redux-devtools-extension";
-import reducer from "./reducer";
-
-const store = createStore(reducer, devToolsEnhancer({ trace: true }));
-
-export default store;
