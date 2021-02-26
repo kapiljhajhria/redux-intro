ea5015ce39f69ff3474965e8936588b2ad33a762
kapil jhajhria <jhajhria44@gmail.com>
2021-02-26 22:20:51 +0530

    refactored code

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -13,45 +13,20 @@ let lastId = 0;
 export default createReducer([], {
   //key value pairs
   //actions:function(event=>event handler)
-  bugAdded: (state, action) => {
+  //using actions from created action.type to avoid issue when changing names
+  [bugAdded.type]: (state, action) => {
     state.push({
       id: ++lastId,
       description: action.payload.description,
       resolved: false,
     });
   },
-  bugResolved: (state, action) => {
+  [bugResolved.type]: (state, action) => {
     const index = state.findIndex((bug) => bug.id === action.payload.id);
     state[index].resolved = true;
   },
-  bugRemoved: (state, action) => {
+  [bugRemoved.type]: (state, action) => {
     const index = state.findIndex((bug) => bug.id === action.payload.id);
     state.splice(index, 1);
   },
 });
-
-// export default function reducer(state = [], action) {
-//   switch (action.type) {
-//     case bugAdded.type: {
-//       return [
-//         ...state,
-//         {
-//           id: ++lastId,
-//           description: action.payload.description,
-//           resolved: false,
-//         },
-//       ];
-//     }
-//     case bugRemoved.type: {
-//       return [...state.filter((bug) => bug.id !== action.payload.id)];
-//     }
-
-//     case bugResolved.type: {
-//       return state.map((bug) =>
-//         bug.id !== action.payload.id ? bug : { ...bug, resolved: true }
-//       );
-//     }
-//     default:
-//       return state;
-//   }
-// }
