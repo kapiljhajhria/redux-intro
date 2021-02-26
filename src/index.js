25f15f28736f4d9710b1d83d0fe1b27063a5af19
kapil jhajhria <jhajhria44@gmail.com>
2021-02-26 15:40:26 +0530

    using redux extension for debugging

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-// import store from "./store";
-// import * as actions from "./actionTypes";
-// import { bugAdded, bugRemoved, bugResolved } from "./actionsCreators";
-// const usubscribe = store.subscribe(() => {
-//   console.log("state changed", store.getState());
-// });
+import store from "./store";
+import * as actions from "./actionTypes";
+import { bugAdded, bugRemoved, bugResolved } from "./actionsCreators";
+const usubscribe = store.subscribe(() => {
+  console.log("state changed", store.getState());
+});
 
-// // console.log(store.getState());
+// console.log(store.getState());
 
 // store.dispatch(bugAdded("bug 01"));
 // // console.log(store.getState());
@@ -14,20 +14,3 @@
 // store.dispatch(bugRemoved(1));
 // // console.log(store.getState());
 // store.dispatch(bugResolved(2));
-
-// import store from "./customeStore";
-
-// store.state = 1;
-
-// // console.log(store.state);
-// console.log(store);
-
-import store from "./customeStore";
-import * as actions from "./actionsCreators";
-store.subscribe(() => {
-  console.log("store changed", store.getState());
-});
-
-store.dispatch(actions.bugAdded("bug 001"));
-
-console.log(store.getState());
