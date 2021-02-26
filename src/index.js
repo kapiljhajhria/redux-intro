5c1e7ac6d2ab9816ce34bbaf5dcc1f644937dc29
kapil jhajhria <jhajhria44@gmail.com>
2021-02-26 16:03:24 +0530

    tracing using vscode on linux

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ const usubscribe = store.subscribe(() => {
 
 // console.log(store.getState());
 
-// store.dispatch(bugAdded("bug 01"));
-// // console.log(store.getState());
-// // usubscribe();
-// store.dispatch(bugAdded("bug 02"));
-// store.dispatch(bugRemoved(1));
-// // console.log(store.getState());
-// store.dispatch(bugResolved(2));
+store.dispatch(bugAdded("bug 01"));
+// console.log(store.getState());
+// usubscribe();
+store.dispatch(bugAdded("bug 02"));
+store.dispatch(bugRemoved(1));
+// console.log(store.getState());
+store.dispatch(bugResolved(2));
