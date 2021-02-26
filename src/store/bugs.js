b0ec412f46218ea02b8071774b2ac4d156732b91
kapil jhajhria <jhajhria44@gmail.com>
2021-02-26 21:33:33 +0530

    create actions using toolkit

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,38 +1,46 @@
-//action types
+import { createAction } from "@reduxjs/toolkit";
 
-const BUG_ADDED = "bugAdded";
-const BUG_REMOVED = "bugRemoved";
-const BUG_RESOLVED = "bugResolved";
+// const bugUpdated = createAction("bugUpdated");
+// console.log(bugUpdated(1));
+
+// //action types
+
+// const BUG_ADDED = "bugAdded";
+// const BUG_REMOVED = "bugRemoved";
+// const BUG_RESOLVED = "bugResolved";
 
 // Action creators
 
-export function bugAdded(descriptiopn) {
-  return {
-    type: BUG_ADDED,
-    payload: { description: descriptiopn },
-  };
-}
+// export function bugAdded(descriptiopn) {
+//   return {
+//     type: BUG_ADDED,
+//     payload: { description: descriptiopn },
+//   };
+// }
+export const bugAdded = createAction("bugAdded");
 
-export function bugRemoved(id) {
-  return {
-    type: BUG_REMOVED,
-    payload: { id: id },
-  };
-}
+// export function bugRemoved(id) {
+//   return {
+//     type: BUG_REMOVED,
+//     payload: { id: id },
+//   };
+// }
+export const bugRemoved = createAction("bugRemoved");
 
-export function bugResolved(id) {
-  return {
-    type: BUG_RESOLVED,
-    payload: { id: id },
-  };
-}
+// export function bugResolved(id) {
+//   return {
+//     type: BUG_RESOLVED,
+//     payload: { id: id },
+//   };
+// }
+export const bugResolved = createAction("bugResolved");
 
 //reducer
 
 let lastId = 0;
 export default function reducer(state = [], action) {
   switch (action.type) {
-    case BUG_ADDED: {
+    case bugAdded.type: {
       return [
         ...state,
         {
@@ -42,11 +50,11 @@ export default function reducer(state = [], action) {
         },
       ];
     }
-    case BUG_REMOVED: {
+    case bugRemoved.type: {
       return [...state.filter((bug) => bug.id !== action.payload.id)];
     }
 
-    case BUG_RESOLVED: {
+    case bugResolved.type: {
       return state.map((bug) =>
         bug.id !== action.payload.id ? bug : { ...bug, resolved: true }
       );
